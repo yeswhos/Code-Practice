90905ec6a59cc33213fcaf56f4f3dd59fddd38fb
yeswhos <yeswhos@outlook.com>
2020-04-04 08:03:02 +0100

    python practice

---
+++ python/Day01/demo00.py
@@ -0,0 +1,14 @@
+str = '1000268201_693b08cb0e.jpg#0\tA child in a pink dress is climbing up a set of stairs in an entry way .'
+# a = []
+# str_new = ""
+# for i in range(len(str)):
+#     if(str[i] == "#"):
+#         #print("\\t")
+#         for j in range(i + 3, len(str)):
+            
+str_new = str[28:].split(" ")
+          
+print(str_new)
+
+#str_new.split(" ")
+#print(str_new)
