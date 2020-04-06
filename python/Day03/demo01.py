941115279b297dc2ac3a6c6c65e6754f5b327f53
yeswhos <yeswhos@outlook.com>
2020-04-06 10:21:36 +0100

    count word update

--- python/Day03/demo01.py
+++ python/Day03/demo01.py
@@ -4,9 +4,13 @@ list1 = ['A child in a pink dress is climbing up a set of stairs in an entry way
 counts = {}#创建一个空的字典
 for i in range(0, 3):
     str = "".join(list1[i])
+    str = str.lower()
+    print(str)
     words = str.split()
+    
     #print(words)
     for word in words:
+
         counts[word] = counts.get(word,0)+1
     #print(counts)
     items = list(counts.items())#将字典中的每对键值对看做一个元素存入items列表中
@@ -15,6 +19,10 @@ for i in range(0, 3):
 list2 = []
 for i in range(len(items)):
     word,count = items[i]
-    if count > 3:
+    if (count > 1) & (word != '.') & (word != ','):
         list2.append(word)
+            
+    
+    print(word, count)
+
 print(list2)
