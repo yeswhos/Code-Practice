ea36784e8fd64a6a7ab6dd48ad697c2ed6d34c42
yeswhos <yeswhos@outlook.com>
2020-04-19 09:25:23 +0100

    framework of card_main

---
+++ python/demo02_card_system/card_main.py
@@ -0,0 +1,11 @@
+account_str = input("Select operation: ")
+print("Your choice is: %s" % account_str)
+
+if account_str in [1, 2, 3]:
+    pass
+
+elif account_str == 0:
+    pass
+
+else:
+    print("invalid choice, select again please")
