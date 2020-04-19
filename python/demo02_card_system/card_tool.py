b7a0079974220c50cf2d448365d366b2fcc3e356
yeswhos <yeswhos@outlook.com>
2020-04-19 09:53:06 +0100

    add new card completed

--- python/demo02_card_system/card_tool.py
+++ python/demo02_card_system/card_tool.py
@@ -1,3 +1,5 @@
+card_list = []
+
 def show_menu():
     '''显示菜单'''
     print("*" * 50)
@@ -11,7 +13,19 @@ def show_menu():
 def new_card():
     """新增名片"""
     print("-" * 50)
-    print("add new card")
+    print("Add new card")
+    name = input("Please enter name: ")
+    phone = input("Please enter phone number: ")
+    qq = input("Please enter qq: ")
+    email = input("Please enter email: ")
+    card_dict = {
+        "name" : name,
+        "phone": phone,
+        "qq" : qq,
+        "email" : email
+    }
+    card_list.append(card_dict)
+    print("添加 %s 成功" % name)
 
 def show_all():
     """显示所有名片"""
