f656324e46cb019f3e8a0fb6afef1a434b0b1e74
yeswhos <yeswhos@outlook.com>
2020-04-19 10:05:10 +0100

    show all function

--- python/demo02_card_system/card_tool.py
+++ python/demo02_card_system/card_tool.py
@@ -14,23 +14,28 @@ def new_card():
     """新增名片"""
     print("-" * 50)
     print("Add new card")
-    name = input("Please enter name: ")
-    phone = input("Please enter phone number: ")
+    name_str = input("Please enter name: ")
+    phone_str = input("Please enter phone number: ")
     qq = input("Please enter qq: ")
     email = input("Please enter email: ")
     card_dict = {
-        "name" : name,
-        "phone": phone,
+        "name" : name_str,
+        "phone": phone_str,
         "qq" : qq,
         "email" : email
     }
     card_list.append(card_dict)
-    print("添加 %s 成功" % name)
+    print("添加 %s 成功" % name_str)
 
 def show_all():
     """显示所有名片"""
     print("-" * 50)
     print("Show all")
+    for name in ["name", "phone", "qq", "email"]:
+        print(name, end = "\t\t")
+    print(" ")
+    for card in card_list:
+        print("%s\t\t%s\t\t%s\t\t%s" % (card["name"], card["phone"], card["qq"], card["email"]))
 
 def search_card():
     """搜索名片"""
