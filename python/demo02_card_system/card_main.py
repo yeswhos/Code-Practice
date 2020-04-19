9afe2ae510fe4a72b3cde1ee3ea353f791465db5
yeswhos <yeswhos@outlook.com>
2020-04-19 09:41:10 +0100

    add show_menu function

--- python/demo02_card_system/card_main.py
+++ python/demo02_card_system/card_main.py
@@ -1,10 +1,19 @@
+import card_tool
+
 while True:
+    # TODO(Fanhui) 显示功能菜单
+    card_tool.show_menu()
 
     account_str = input("Select operation: ")
     print("Your choice is: %s" % account_str)
 
     if account_str in ["1", "2", "3"]:
-        pass
+        if account_str == "1":
+            pass
+        elif account_str == "2":
+            pass
+        elif account_str == "3":
+            pass
 
     elif account_str == "0":
         print("Thanks for using, see you later")
---
+++ python/demo02_card_system/card_tool.py
@@ -0,0 +1,9 @@
+def show_menu():
+    '''显示菜单'''
+    print("*" * 50)
+    print("Welcome")
+    print("1. Add new card")
+    print("2. Show hands")
+    print("3. Search for card")
+    print("0. Exit")
+    print("*" * 50)
