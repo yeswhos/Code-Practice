b0a177f9ad47c1da9ae7baafeb332a7cd53404d7
yeswhos <yeswhos@outlook.com>
2020-04-22 18:44:49 +0100

    reinforcement for loop

--- Pratice/src/test/meng/demo25Collection/Demo01Iterator.java
+++ Pratice/src/test/meng/demo25Collection/Demo01Iterator.java
@@ -5,11 +5,14 @@ import java.util.Iterator;
 
 public class Demo01Iterator {
     public static void main(String[] args) {
+
         ArrayList<Integer> coll = new ArrayList<>();
         for(int i = 0; i < 5; i++){
             coll.add(i);
         }
+        //多态，获取迭代器的实现对象，然后把指针或者索引指向集合的-1索引
         Iterator<Integer> iterator1 = coll.iterator();
+        //判断有没有下一个
         while(iterator1.hasNext()){
             int a = iterator1.next();
             System.out.println(a);
---
+++ Pratice/src/test/meng/demo25Collection/Demo02For.java
@@ -0,0 +1,28 @@
+package test.meng.demo25Collection;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Demo02For {
+    public static void main(String[] args) {
+        int [] a = {1, 2, 3, 4};
+        ArrayList<String> b = new ArrayList<>();
+        b.add("1");
+        b.add("2");
+        b.add("3");
+        b.add("4");
+        demo00(a);
+        demo01(b);
+
+    }
+    private static void demo00(int [] a){
+        for(int i: a){
+            System.out.println("array: " + i);
+        }
+    }
+    private static void demo01(ArrayList<String> a){
+        for(String i : a){
+            System.out.println("arraylist: " + i);
+        }
+    }
+}
