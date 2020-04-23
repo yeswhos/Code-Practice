82937746bafd3b59fed42fbbf62147cd1d1e9712
yeswhos <yeswhos@outlook.com>
2020-04-23 11:06:40 +0100

    submit two sum problem

--- LeetCode/src/problem/TwoSum/Sum.java
+++ LeetCode/src/problem/TwoSum/Sum.java
@@ -1,20 +1,34 @@
 package problem.TwoSum;
 
+import java.util.ArrayList;
+
 public class Sum{
     public static void main(String[] args) {
+        int [] arr = {2, 7, 11, 15};
+        int target = 9;
+        int [] result = twoSum(arr, target);
+        for(int i = 0; i < result.length; i++){
+            System.out.println(i);
+        }
 
     }
-    public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length(); i++){
+    public static int[] twoSum(int[] nums, int target) {
+        int result [] = new int [2];
+        for(int i = 0; i < nums.length; i++){
             int a = nums[i];
-            for(int j = 0; j < nums.length(); j++){
-                if(j != i){
-                    int b = nums[j];
-                }
+            for(int j = 0; j < nums.length && j != i; j++){
+                int b = nums[j];
+
                 if(a + b == target){
-                    return i, j;
+//                    System.out.println(i);
+//                    System.out.println(j);
+                    result[0] = i;
+                    result[1] = j;
+                    break;
                 }
             }
         }
+
+        return result;
     }
 }
