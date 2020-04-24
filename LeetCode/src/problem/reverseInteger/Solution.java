925f156c0a65d8234d22e49939fddf0892c64521
yeswhos <yeswhos@outlook.com>
2020-04-24 10:25:19 +0100

    reverse problem v2

--- LeetCode/src/problem/reverseInteger/Solution.java
+++ LeetCode/src/problem/reverseInteger/Solution.java
@@ -4,19 +4,26 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
-        int x = -123;
+        int x = 1534236469;
         int result = reverse(x);
         System.out.println(result);
     }
     public static int reverse(int x){
-        int len = (x + "").length();
+        int len = 0;
+        if(x < 0){
+            len = (x + "").length() - 1;
+        }
+        else {
+            len = (x + "").length();
+        }
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < len; i++){
-
             int j = x % 10;
             x = x / 10;
             list.add(j);
+
         }
+        System.out.println(list);
         int result = 0;
         for(int i = 0; i < list.size(); i++){
             double a = Math.pow(10, len - i - 1);
