05ec9f8fa3b90928162d161c3cc613172659fe19
yeswhos <yeswhos@outlook.com>
2020-04-26 07:46:51 +0100

    muliti objects

--- python/demo05_Object_oriented/demo01_two_class.py
+++ python/demo05_Object_oriented/demo01_two_class.py
@@ -5,9 +5,8 @@ class Cat:
         print("drink")
 
 tom = Cat()
-tom.eat()
-tom.drink()
-
 print(tom)
-addr = id(tom)
-print("十六进制 %x" % addr)
+lazy_tom = Cat()
+print(lazy_tom)
+lazy = lazy_tom
+print(lazy)
