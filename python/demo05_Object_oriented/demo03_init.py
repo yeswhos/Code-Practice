121ae7608045f08c4c0e0f20609272ba96d01dc3
yeswhos <yeswhos@outlook.com>
2020-04-27 08:35:15 +0100

    init parameter

--- python/demo05_Object_oriented/demo03_init.py
+++ python/demo05_Object_oriented/demo03_init.py
@@ -1,6 +1,9 @@
 class Cat:
-    def __init__(self):
-        self.name = "Tom"
+    def __init__(self, new_name):
+        #self.name = "Tom"
+        self.name = new_name
 
-tom = Cat()
+tom = Cat("Tom")
 print(tom.name)
+lazy = Cat("Lazy")
+print(lazy.name)
