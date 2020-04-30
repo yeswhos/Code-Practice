1b515f0f3154e589ca12bee20889d9b5a0e3aa69
yeswhos <yeswhos@outlook.com>
2020-04-30 19:28:59 +0100

    gun case: gun class

---
+++ python/demo05_Object_oriented/case_gun/demo09_gun.py
@@ -0,0 +1,19 @@
+class Gun:
+    def __init__(self, model):
+        self.model = model
+        self.bullet_count = 0
+
+    def add_bullet(self, count):
+        self.bullet_count += count
+
+    def shoot(self):
+        if(self.bullet_count <= 0):
+            print("%s no bullet" % self.model)
+            return
+        self.bullet_count -= 1
+        print("%s biubiubiu" % self.model)
+        print("left bullet: %s" % self.bullet_count)
+
+ak47 = Gun("ak47")
+ak47.add_bullet(30)
+ak47.shoot()
