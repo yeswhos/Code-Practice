60e5f94cd0f870906ad062f8776e332fc8c4e095
yeswhos <yeswhos@outlook.com>
2020-04-30 17:13:29 +0100

    house item class

---
+++ python/demo05_Object_oriented/case_house/demo07_house_item.py
@@ -0,0 +1,13 @@
+class HouseItem:
+    def __init__(self, name, size):
+        self.name = name;
+        self.size = size;
+    def __str__(self):
+        return "[%s] occupy %.2f room" % (self.name, self.size)
+
+bed = HouseItem("席梦思", 10)
+table = HouseItem("餐桌", 4)
+
+print(bed)
+print(table)
+
