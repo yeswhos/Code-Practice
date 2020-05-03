fbe28593e2bda4a80d2bccad9b72d2a5ab3c9f06
yeswhos <yeswhos@outlook.com>
2020-05-03 08:40:32 +0100

    overwrite extend

---
+++ python/demo06/demo04_extend.py
@@ -0,0 +1,19 @@
+class Animal:
+    def eat(self):
+        print("eat")
+    def drink(self):
+        print("drink")
+
+class Dog(Animal):
+    def bark(self):
+        print("bark")
+
+class XiaoTianQuan(Dog):
+    def fly(self):
+        print("fly")
+    def bark(self):
+        super().bark()
+        print("overwrite bark")
+
+xtq = XiaoTianQuan()
+xtq.bark()
