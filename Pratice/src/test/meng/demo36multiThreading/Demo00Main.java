be19096b677ffcce33d4fc04834872dff010743b
yeswhos <yeswhos@outlook.com>
2020-05-05 08:52:07 +0100

    multi thread

--- Pratice/src/test/meng/demo36multiThreading/Demo00Main.java
+++ Pratice/src/test/meng/demo36multiThreading/Demo00Main.java
@@ -1,4 +1,11 @@
 package test.meng.demo36multiThreading;
 
-public class Demo00Main {
+public class Demo00Main{
+    public static void main(String[] args) {
+        Thread mt = new Demo01();
+        mt.start();
+        for(int i = 0; i < 10; i++){
+            System.out.println("main: " + i);
+        }
+    }
 }
