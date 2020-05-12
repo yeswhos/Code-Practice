b0b8d77230167f397ef1b5888066abc52d00ca5f
yeswhos <yeswhos@outlook.com>
2020-05-12 18:47:59 +0100

    lambda no return and parameter

---
+++ Pratice/src/test/meng/demo40ThreadAndLambda/Cook.java
@@ -0,0 +1,5 @@
+package test.meng.demo40ThreadAndLambda;
+
+public interface Cook {
+    public abstract void makeFood();
+}
---
+++ Pratice/src/test/meng/demo40ThreadAndLambda/Demo04Lambda01.java
@@ -0,0 +1,20 @@
+package test.meng.demo40ThreadAndLambda;
+
+public class Demo04Lambda01{
+    public static void main(String[] args) {
+        invokeCook(new Cook() {
+            @Override
+            public void makeFood() {
+                System.out.println("匿名内部类");
+            }
+        });
+
+        invokeCook(()->{
+            System.out.println("lambda");
+        });
+    }
+
+    public static void invokeCook(Cook cook) {
+        cook.makeFood();
+    }
+}
