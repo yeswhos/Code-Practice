06278559c949cef8741d0db36e99a2b1a31a614a
yeswhos <yeswhos@outlook.com>
2020-05-12 18:21:26 +0100

    lambda

---
+++ Pratice/src/test/meng/demo40ThreadAndLambda/Demo03Lambda.java
@@ -0,0 +1,9 @@
+package test.meng.demo40ThreadAndLambda;
+
+public class Demo03Lambda {
+    public static void main(String[] args) {
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName() + "线程运行");
+        }).start();
+    }
+}
