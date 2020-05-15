abc841a96b17eed25d3dbdcd46f3114ef877eaca
yeswhos <yeswhos@outlook.com>
2020-05-15 08:31:25 +0100

    inner anonymous and lambda for file filter

--- Pratice/src/test/meng/demo42Recursion/Demo05Fliter.java
+++ Pratice/src/test/meng/demo42Recursion/Demo05Fliter.java
@@ -1,10 +1,24 @@
 package test.meng.demo42Recursion;
 
 import java.io.File;
+import java.io.FileFilter;
 
 public class Demo05Fliter {
     public static void getAllRecurseFile(File dir){
-        File [] arr = dir.listFiles(new FileFilterD());
+//        File [] arr = dir.listFiles(new FileFilterD());
+        //---------------------------------------------
+//        File [] arr = dir.listFiles(new FileFilter() {
+//            @Override
+//            public boolean accept(File pathname) {
+//                return pathname.isDirectory() || pathname.toString().toLowerCase().endsWith(".java");
+//            }
+//        });
+        //-------------------------------------------------
+//        File [] arr = dir.listFiles((File pathname)->{
+//            return pathname.isDirectory() || pathname.toString().toLowerCase().endsWith(".java");
+//        });
+//        -------------------------------------------------------
+        File [] arr = dir.listFiles((pathname)-> pathname.isDirectory() || pathname.toString().toLowerCase().endsWith(".java"));
         for(File f : arr){
             //String s = f.toString();
             if(f.isDirectory()){
