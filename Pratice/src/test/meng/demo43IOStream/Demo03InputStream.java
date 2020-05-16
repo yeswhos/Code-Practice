c71ae0938595ac7cb5391ee82f91ca3b13d80ed2
yeswhos <yeswhos@outlook.com>
2020-05-16 19:38:02 +0100

    input stream

---
+++ Pratice/src/test/meng/demo43IOStream/Demo03InputStream.java
@@ -0,0 +1,17 @@
+package test.meng.demo43IOStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Demo03InputStream{
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("C:\\Users\\yeswhos\\Desktop\\桌面不常用\\a.txt");
+//        int len = fis.read();
+//        System.out.println((char)len);
+        int len = 0;
+        while((len = fis.read()) != -1){
+//            len = fis.read();
+            System.out.println((char)len);
+        }
+    }
+}
