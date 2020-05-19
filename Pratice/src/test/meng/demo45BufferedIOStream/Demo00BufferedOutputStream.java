3c25b932f6be79f4be354b196282ffd33e2aecbc
yeswhos <yeswhos@outlook.com>
2020-05-19 08:24:02 +0100

    buffered output stream

---
+++ Pratice/src/test/meng/demo45BufferedIOStream/Demo00BufferedOutputStream.java
@@ -0,0 +1,15 @@
+package test.meng.demo45BufferedIOStream;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Demo00BufferedOutputStream {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\yeswhos\\Desktop\\桌面不常用\\g.txt");
+        BufferedOutputStream bof = new BufferedOutputStream(fos);
+        bof.write("BufferedOutputStream test".getBytes());
+        bof.flush();
+        bof.close();
+    }
+}
