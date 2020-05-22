682337ca03b838a9d7aa578b9a39a3577527545a
yeswhos <yeswhos@outlook.com>
2020-05-22 08:43:39 +0100

    serial UID

--- Pratice/src/test/meng/demo46ObjectInOutput/Person.java
+++ Pratice/src/test/meng/demo46ObjectInOutput/Person.java
@@ -3,6 +3,7 @@ package test.meng.demo46ObjectInOutput;
 import java.io.Serializable;
 
 public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name;
     private int age;
 
