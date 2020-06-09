87133be0f4710e050b0d6aecec5a01d204777af5
yeswhos <yeswhos@outlook.com>
2020-06-09 08:42:58 +0100

    address pass

---
+++ C++/Demo01/Demo01/Demo04.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void swap01(int* p1, int* p2) {
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+int main() {
+	int a = 10;
+	int b = 20;
+	swap01(&a, &b);
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	system("pause");
+	return 0;
+}
