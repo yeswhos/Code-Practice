28d1f0cfec27f0fa5a7965ad9bbeeb9b2eabab43
yeswhos <yeswhos@outlook.com>
2020-06-12 09:14:18 +0100

    struct within struct

---
+++ C++/Demo01/Demo01/Demo09StructWithStruct.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Student {
+	string name;
+	int age;
+};
+
+struct Teacher {
+	string name;
+	int age;
+	Student stu;
+};
+
+int main() {
+	Teacher t;
+	t.name = "Fanhui";
+	t.age = 23;
+	t.stu.name = "Lulu";
+	t.stu.age = 23;
+	Teacher* p = &t;
+	cout << "Teacher " << t.name << t.age << "\n" << " Student " << p->stu.name << t.stu.age << endl;
+}
