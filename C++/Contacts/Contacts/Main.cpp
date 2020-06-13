e6e6233994946a059662d0302dcf1893e7b953a9
yeswhos <yeswhos@outlook.com>
2020-06-13 09:04:21 +0100

    example add person function

--- C++/Contacts/Contacts/Main.cpp
+++ C++/Contacts/Contacts/Main.cpp
@@ -25,13 +25,63 @@ struct AddressBook {
 	Person p[MAX];
 	int len;
 };
+
+void addPerson(AddressBook* abs) {
+	if (abs->len == MAX) {
+		cout << "Already full, cannot add" << endl;
+		return;
+	}
+	else {
+		string name;
+		cout << "Please input name" << endl;
+		cin >> name;
+		abs->p[abs->len].Name = name;
+
+		int sex;
+		cout << "Please input sex" << endl;
+		cout << "0 is female, 1 is male" << endl;
+		cin >> sex;
+		while (true)
+		{
+			if (sex == 1 || sex == 0) {
+				abs->p[abs->len].Sex = sex;
+				break;
+			}
+			else {
+				cout << "Please re-input sex";
+			}
+		}
+
+		int age;
+		cout << "Please input age" << endl;
+		cin >> age;
+		abs->p[abs->len].age = age;
+
+		string number;
+		cout << "Please input number" << endl;
+		cin >> number;
+		abs->p[abs->len].phoneNumber = number;
+
+		string address;
+		cout << "Please input address" << endl;
+		cin >> address;
+		abs->p[abs->len].Address = address;
+		abs->len++;
+		cout << "Ìí¼Ó³É¹¦" << endl;
+		system("pause");
+		system("cls");
+	}
+}
 int main() {
 	int select = 0;
+	AddressBook abs;
+	abs.len = 0;
 	while (true) {
 		showMenu();
 		cin >> select;
 		switch (select) {
 			case 1:
+				addPerson(&abs);
 				break;
 			case 2:
 				break;
