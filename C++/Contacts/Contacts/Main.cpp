e2e476d1b088bdf883e2563deb3cdcc3795d75ae
yeswhos <yeswhos@outlook.com>
2020-06-13 08:31:02 +0100

    example exit function

--- C++/Contacts/Contacts/Main.cpp
+++ C++/Contacts/Contacts/Main.cpp
@@ -3,18 +3,42 @@ using namespace std;
 
 void showMenu() {
 	cout << "**********************************" << endl;
-	cout << "Add Contact" << endl;
-	cout << "Show Contacts" << endl;
-	cout << "Delete Contacts" << endl;
-	cout << "Search Contact" << endl;
-	cout << "Modify Contact" << endl;
-	cout << "Clear All Contact" << endl;
-	cout << "Exit" << endl;
+	cout << "1. Add Contact" << endl;
+	cout << "2. Show Contacts" << endl;
+	cout << "3. Delete Contacts" << endl;
+	cout << "4. Search Contact" << endl;
+	cout << "5. Modify Contact" << endl;
+	cout << "6. Clear All Contact" << endl;
+	cout << "0. Exit" << endl;
 	cout << "**********************************" << endl;
 }
 
 int main() {
-	showMenu();
-	system("pause");
-	return 0;
+	int select = 0;
+	while (true) {
+		showMenu();
+		cin >> select;
+		switch (select) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 0:
+				cout << "See ya" << endl;
+				system("pause");
+				return 0;
+				break;
+			default:
+				break;
+		}
+	}
+
 }
