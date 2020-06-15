ba6244391ea5af2937fb215c79042a1f682eb536
yeswhos <yeswhos@outlook.com>
2020-06-15 09:17:44 +0100

    heap area example

--- C++/Demo02/Demo02/Demo02HeapArea.cpp
+++ C++/Demo02/Demo02/Demo02HeapArea.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int* func() {
-	int a = 10;
-	return &a;
+	int *p = new int(10);
+	return p;
 }
 
 int main() {
 	int* p = func();
 	cout << *p << endl;
-	cout << *p << endl;
+	//cout << *p << endl;
 	system("pause");
 	return 0;
 }
