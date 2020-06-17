bdbd3267f4c9aa1cf30181ea26e1a37bc987ad67
yeswhos <yeswhos@outlook.com>
2020-06-17 09:15:25 +0100

    cite 2

--- C++/Demo02/Demo02/Demo05Cite2.cpp
+++ C++/Demo02/Demo02/Demo05Cite2.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main() {
 	int a = 10;
+	int c = 20;
+	//int& b;
 	int& b = a;
 	cout << a << endl;
 	cout << b << endl;
 	b = 20;
+	//传递值不是地址
+	b = c;
 	cout << a << endl;
 	cout << b << endl;
 	system("pause");
