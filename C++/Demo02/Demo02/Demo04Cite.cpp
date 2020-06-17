a126950ecc92fb5283f6aef82c19a916e096617e
yeswhos <yeswhos@outlook.com>
2020-06-17 08:59:19 +0100

    cite init

---
+++ C++/Demo02/Demo02/Demo04Cite.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a = 10;
+	int& b = a;
+	cout << a << endl;
+	cout << b << endl;
+	b = 20;
+	cout << a << endl;
+	cout << b << endl;
+}
