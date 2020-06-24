f26a76ecaaa0a3e3f97c62e3d7117c77dd2a20d1
yeswhos <yeswhos@outlook.com>
2020-06-24 08:32:24 +0100

    __name__

--- python/demo08_module/demo03_import_from.py
+++ python/demo08_module/demo03_import_from.py
@@ -1,3 +1,4 @@
 from demo01_test import sayHello
-
 sayHello()
+
+import  demo04_name as name
---
+++ python/demo08_module/demo04_name.py
@@ -0,0 +1,7 @@
+def say_hello():
+    print("hello")
+
+if __name__ == "__main__":
+    print(__name__)
+    print("Fanhui developed")
+    say_hello()
