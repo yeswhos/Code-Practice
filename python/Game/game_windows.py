498ae1e5aaf7adbb11c9b9932c9ddfeb9b2f2455
yeswhos <yeswhos@outlook.com>
2020-06-28 08:01:05 +0100

    set mode, screen

---
+++ python/Game/game_windows.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+#set_mode(resolution = (0, 0), flags = 0, depth = 0)
+#resolution屏幕宽和高
+#flag是否全屏等等
+#depth颜色的位数，自动匹配
+
+screen = pygame.display.set_mode((480, 700))
+
+while True:
+    pass
+
+pygame.quit()
