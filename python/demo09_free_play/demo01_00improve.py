b1329dd2849ebf05d4253953a8a466394d6a6b28
yeswhos <yeswhos@outlook.com>
2020-07-11 09:04:03 +0100

    algorithm improve

---
+++ python/demo09_free_play/demo01_00improve.py
@@ -0,0 +1,11 @@
+#如果a + b + c == 1000, a ^ 2 + b ^ 2 = c ^ 2，求所有abc可能的组合
+import math
+def combination(num):
+    for a in range(0, num + 1):
+        for b in range(0, num + 1):
+
+            c = 1000 - a - b
+            if (math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2)):
+                    print(a, "\t", b,  "\t", c)
+
+combination(1000)
