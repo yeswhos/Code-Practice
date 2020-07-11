3fd624c0568b08d80aae27c16afff9b9b83aa1e4
yeswhos <yeswhos@outlook.com>
2020-07-11 09:05:20 +0100

    add time comparsion

--- python/demo09_free_play/demo01_00improve.py
+++ python/demo09_free_play/demo01_00improve.py
@@ -1,5 +1,7 @@
 #如果a + b + c == 1000, a ^ 2 + b ^ 2 = c ^ 2，求所有abc可能的组合
 import math
+import time
+
 def combination(num):
     for a in range(0, num + 1):
         for b in range(0, num + 1):
@@ -8,4 +10,6 @@ def combination(num):
             if (math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2)):
                     print(a, "\t", b,  "\t", c)
 
+start_time = time.time()
+end_time = time.time()
 combination(1000)
