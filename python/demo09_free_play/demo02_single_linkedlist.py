d5c58c56386dcc12bb39a95b8e39a7b304a6c9a7
yeswhos <yeswhos@outlook.com>
2020-07-14 07:58:59 +0100

    single linked list init

---
+++ python/demo09_free_play/demo02_single_linkedlist.py
@@ -0,0 +1,35 @@
+class Node(object):
+    def __init__(self, elem):
+        self.elem = elem
+        self.next = None
+
+class SingleLinkList(object):
+    def __init__(self):
+        self.__head = None
+
+    def is_empty(self):
+        pass
+
+    def length(self):
+        pass
+
+    def travel(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def append(self, item):
+        pass
+
+    def insert(self, pos, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def search(self, item):
+        pass
+
+node = Node(100)
+single_obj = SingleLinkList()
