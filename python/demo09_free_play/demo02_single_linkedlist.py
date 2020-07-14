c858dc97886a4b257901ed3558f4b66855808721
yeswhos <yeswhos@outlook.com>
2020-07-14 08:27:09 +0100

    is empty, length, travel, append function

--- python/demo09_free_play/demo02_single_linkedlist.py
+++ python/demo09_free_play/demo02_single_linkedlist.py
@@ -4,23 +4,42 @@ class Node(object):
         self.next = None
 
 class SingleLinkList(object):
-    def __init__(self):
-        self.__head = None
+    def __init__(self, node = None):
+        self._head = None
 
     def is_empty(self):
-        pass
+        cur = self._head
+        if cur:
+            return True
+        else:
+            return False
 
     def length(self):
-        pass
+        cur = self._head
+        count = 1
+        while cur != None:
+            count += 1
+            cur = cur.next
+        return count
 
     def travel(self):
-        pass
+        cur = self._head
+        while cur != None:
+            print(cur.elem)
+            cur = cur.next
 
     def add(self, item):
         pass
 
     def append(self, item):
-        pass
+        node = Node(item)
+        if self.is_empty():
+            self._head = node
+        else:
+            cur = self._head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
 
     def insert(self, pos, item):
         pass
@@ -33,3 +52,4 @@ class SingleLinkList(object):
 
 node = Node(100)
 single_obj = SingleLinkList()
+#差个测试
