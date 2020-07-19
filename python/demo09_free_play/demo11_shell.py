9c84dd12b56742ebdd77178f705ccfcedced68b0
yeswhos <yeswhos@outlook.com>
2020-07-19 08:26:21 +0100

    shell, not completed

---
+++ python/demo09_free_play/demo11_shell.py
@@ -0,0 +1,18 @@
+def shell(list):
+    n = len(list)
+    gap = n // 2
+    while (gap > 0):
+        for i in range (gap, n):
+            j = i
+            while (i > 0):
+                if (list[j] > list[j - gap]):
+                    list[j], list[j - gap] = list[j - gap], list[j]
+                    j += 1
+                else:
+                    break
+            i += 1
+        gap = gap // 2
+
+list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shell(list)
+print(list)
