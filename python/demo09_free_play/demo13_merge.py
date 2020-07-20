4955f7310c1c1fca855fae193a1d602d2372db1f
yeswhos <yeswhos@outlook.com>
2020-07-20 09:58:31 +0100

    complete merge sorting

--- python/demo09_free_play/demo13_merge.py
+++ python/demo09_free_play/demo13_merge.py
@@ -1,14 +1,28 @@
-def merge(list):
-    n = len(list)
-    midValue = n // 2
-    if (n <= 1):
-        return list
-    right_list = merge(list[:midValue])
-    left_list = merge(list[midValue:])
+def merge(alist):
+    n = len(alist)
+
+    if(n <= 1):
+        return alist
+    mid = n // 2
+    left_list = merge(alist[:mid])
+    right_list = merge(alist[mid:])
 
     left_pointer, right_pointer = 0, 0
     res = []
-    while(left_pointer > len(left_list)) and (right_pointer > len(right_list)):
-        if(left_list[left_pointer] < list[right_list]):
+    while(left_pointer < len(left_list)) and (right_pointer < len(right_list)):
+        if left_list[left_pointer] < right_list[right_pointer]:
+            res.append(left_list[left_pointer])
+            left_pointer += 1
+        else:
+            res.append(right_list[right_pointer])
+            right_pointer += 1
+    # res.append(left_list[left_pointer:])
+    # res.append(right_list[right_pointer:])
+    res += left_list[left_pointer:]
+    res += right_list[right_pointer:]
+    return res
 
-
+if __name__ == "__main__":
+    list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    list = merge(list)
+    print(list)
