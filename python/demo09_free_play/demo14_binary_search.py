58e8448d241c274ab2084320ad3bfea5c6158b5b
yeswhos <yeswhos@outlook.com>
2020-07-21 09:34:47 +0100

    completed binary search v1

--- python/demo09_free_play/demo14_binary_search.py
+++ python/demo09_free_play/demo14_binary_search.py
@@ -1,12 +1,15 @@
 def binary_search(list, item):
     n = len(list)
-    mid = n / 2
-    if(list[mid] == item):
-        return True
-    elif(mid >=1):
+    mid = n // 2
+    if (n > 0):
+        if(list[mid] == item):
+            return True
         if(list[mid] < item):
             return binary_search(list[mid + 1:], item)
         else:
             return binary_search(list[:mid], item)
     return False
 
+if __name__ == "__main__":
+    list = [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    print(binary_search(list, 100))
