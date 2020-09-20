5c3895e004c6f71b7d946d5e63dfb06ae1c4270e
yeswhos <yeswhos@outlook.com>
2020-09-21 07:55:08 +0800

    string index

---
+++ python/demo10_practice/demo04_string.py
@@ -0,0 +1,9 @@
+#字符串索引
+s = 'hello'
+print("index", s.index('lo'))
+print("rindex", s.rindex('lo'))
+print("find", s.find('lo'))
+print("rfind", s.rfind('lo'))
+
+#if string is not found then return -1
+print('not found', s.find('k'))
