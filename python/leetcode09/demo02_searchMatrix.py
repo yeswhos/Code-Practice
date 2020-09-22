e5463c1c8f1f6f665c35884ba76dcb45e2618492
yeswhos <yeswhos@outlook.com>
2020-09-22 17:14:04 +0800

    rewrite and completed search matrix

--- python/leetcode09/demo02_searchMatrix.py
+++ python/leetcode09/demo02_searchMatrix.py
@@ -1,27 +1,34 @@
 def searchMatrix(matrix, target):
     n = len(matrix)
     m = len(matrix[0])
-    i = 0
-    j = 0
-    counter = 0
-    while (target != matrix[i][j]):
-        if target > matrix[i][j]:
-            j += 1
-        if target < matrix[i][j]:
-            j = j - 1
-        if target > matrix[i][j]:
-            i += 1
-        if target < matrix[i][j]:
-            i = j - 1
-        counter += 1
-        if counter >= m * n:
-            return False
-    return True
+    if n == 0 or m == 0:
+        return False
+    for i in range(n):
+        if(target < matrix[i][0]):
+            break
+        if (matrix[i][m - 1] < target):
+            continue
+        if (binarySearch(matrix[i], target) == 1):
+            return True
+    return False
+
+def binarySearch(nums, target):
+    start = 0
+    ends = len(nums) - 1
+    while(start <= ends):
+        mid = (start + ends) // 2
+        if(nums[mid] < target):
+            start = mid + 1
+        elif (nums[mid] > target):
+            ends = mid - 1
+        elif (nums[mid] == target):
+            return 1
+    return -1
 
 matrix = [[1, 4, 7, 11, 15],
           [2, 5, 8, 12, 19],
           [3, 6, 9, 16, 22],
           [10, 13, 14, 17, 24],
           [18, 21, 23, 26, 30]]
-print(searchMatrix(matrix, 20))
+print(searchMatrix(matrix, 21))
 
