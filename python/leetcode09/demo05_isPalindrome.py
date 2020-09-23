2ac6b06afb6dfecc0abe56dc736c59f1064e8ac3
yeswhos <yeswhos@outlook.com>
2020-09-23 09:48:07 +0800

    completed isPalindrome

--- python/leetcode09/demo05_isPalindrome.py
+++ python/leetcode09/demo05_isPalindrome.py
@@ -1,25 +1,14 @@
 def isPalindrome(s):
-    s = s.replace(" ", "")
-    s = s.lower()
-    arr = list(s)
-    print(arr)
-    for i in arr:
-        print(i)
-        num = ord(i)
-        # if(48 <= num <= 57):
-        #     continue
-        # elif(97 <= num <= 122):
-        #     continue
-        # else:
-        #     arr.remove(i)
-        if (num < 48) or (57 < num < 97) or (num > 122):
-            arr.remove(i)
-    print(arr)
-    if (arr == arr[::-1]):
-        return True
-    else:
-        return False
-
+    sgood = ""
+    for ch in s:
+        if (ch.isalnum()):
+            #print(ch)
+            #sgood = "".join(ch.lower())
+            sgood = sgood + ch.lower()
+        print(sgood)
+    #print(sgood[::-1])
+    return sgood == sgood[::-1]
 #s = "A man, a plan, a canal: Panama"
-s = "....a...."
+#s = "....a...."
+s = "race a car"
 print(isPalindrome(s))
