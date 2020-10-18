9ed113fcede4c6a11a2456ec550710a36e2f2539
yeswhos <yeswhos@outlook.com>
2020-10-18 11:15:43 +0800

    single linked list

--- python/demo10_practice/demo09_lnkedlist.py
+++ python/demo10_practice/demo09_lnkedlist.py
@@ -16,8 +16,9 @@ class singleLinkedList(object):
     def length(self):
         cur = self.__head
         count = 0
-        while cur != None:
+        while cur is not None:
             count += 1
+            cur = cur.next
         return count
 
     def travel(self):
@@ -28,13 +29,66 @@ class singleLinkedList(object):
 
     def append(self, item):
         node = Node(item)
+        if self.is_empty():
+            self.__head = node
+        else:
+            cur = self.__head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+
+    def add(self, item):
+        node = Node(item)
+        node.next = self.__head
+        self.__head = node
+
+    def insert(self, pos, item):
+        if pos == 0:
+            self.add(item)
+        elif pos > self.length() - 1:
+            self.append(item)
+        else:
+            node = Node(item)
+            cur = self.__head
+            count = 0
+            while count < pos - 1:
+                count += 1
+                cur = cur.next
+            node.next = cur.next
+            cur.next = node
+
+    def search(self, item):
         cur = self.__head
-        while cur.next != None:
-            cur = cur.next
-        cur.next = node
+        while cur != None:
+            if (cur.elem == item):
+                return True
+            else:
+                cur = cur.next
+        return False
+
+    def remove(self, item):
+        cur = self.__head
+        pre = None
+
+        while cur != None:
+            if cur.elem == item:
+                if cur == self.__head:
+                    self.__head == cur.next
+                else:
+                    pre.next = cur.next
+                    break
+            else:
+                pre = cur
+                cur = cur.next
 
 singleInstance = singleLinkedList()
-# singleInstance.append(10)
-# singleInstance.travel()
+singleInstance.append(10)
+singleInstance.add(20)
+singleInstance.add(20)
+#print(singleInstance.length())
+singleInstance.insert(2, 30)
+print(singleInstance.search(30))
+singleInstance.remove(10)
 singleInstance.travel()
-print(singleInstance.is_empty())
+# singleInstance.travel()
+# print(singleInstance.is_empty())
