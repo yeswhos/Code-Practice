78928d2e5f9901fa1051a09a6cac6235f4343e7c
yeswhos <yeswhos@outlook.com>
2021-05-24 11:56:36 +0800

    0524 webelement find element

--- python/AutoTest/MacPractice/LocateElement.py
+++ python/AutoTest/MacPractice/LocateElement.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-
+import time
 # 创建webdriver对象，指明使用chrome浏览器驱动
 wd = webdriver.Chrome('/Users/mengfanhui/Downloads/chromedriver')
-
+wd.implicitly_wait(10)
 wd.get('https://yeswhos.github.io/')
 
 """
@@ -12,6 +12,8 @@ WebElement也可以调用find element by什么什么方法
 WebDriver选择元素范围是整个web界面
 WebElement选择范围是某个元素的内部
 """
-element = wd.find_element_by_class_name('post-title')
+element = wd.find_element_by_class_name('left')
 text_content = element.find_element_by_tag_name('h2')
 print(text_content.text)
+
+wd.quit()
