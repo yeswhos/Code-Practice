5370de529e2cdffa9380359603ed83e61ecbdb26
yeswhos <yeswhos@outlook.com>
2021-05-25 20:26:25 +0800

    0525 css select

--- python/AutoTest/MacPractice/cssSelect.py
+++ python/AutoTest/MacPractice/cssSelect.py
@@ -8,7 +8,7 @@ wd.get('https://yeswhos.github.io/')
 # 可选择子元素和后代元素
 # 直接下属第一层的是子元素，后好多层的就是后代元素了
 try:
-    element = wd.find_element_by_css_selector('.post-item  .left >.post-title')
+    element = wd.find_element_by_css_selector('.post-item  .left')
     print(element.get_attribute('outerHTML'))
 except Exception as e:
     print(e)
